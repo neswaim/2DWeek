a786f97aaa8eea3877a01492f0966d248702f1c9
neswaim <neswaim@iu.edu>
2021-11-30 12:48:24 -0500

    Added portals

---
+++ Swaim2DWeek/Assets/Scripts/PortalFunc.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalFunc : MonoBehaviour
+{
+
+    public GameObject Portal;
+    public GameObject Player;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Teleport());
+        }
+    }
+
+
+    IEnumerator Teleport()
+    {
+        yield return new WaitForSeconds(3f);
+        Player.transform.position = new Vector2(Portal.transform.position.x, Portal.transform.position.y);
+    }
+}
+
