737ebb54d973012f87588dc361a30df8cb24d565
neswaim <neswaim@iu.edu>
2021-12-07 22:43:48 -0500

    misc changes

---
+++ Swaim2DWeek/Assets/Scripts/Restart.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Restart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(RestartGame());
+        }
+    }
+    public IEnumerator RestartGame()
+    {
+        yield return new WaitForSeconds(6);
+        SceneManager.LoadScene(0);
+    }
+}
